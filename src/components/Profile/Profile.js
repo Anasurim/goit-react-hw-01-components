2f55b7254c54c54263766770adc0fa21bdab8402
Roman Lebedynskyi <108210477+Anasurim@users.noreply.github.com>
2023-04-20 23:59:50 +0200

    create render Profile component without styles

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+
+const Profile = ({ username, tag, location, avatar, stats }) => (
+  <div>
+    <div>
+      <img src={avatar} alt="User avatar" />
+      <p>{username}</p>
+      <p>{tag}</p>
+      <p>{location}</p>
+    </div>
+    <ul>
+      <li>Followers:{stats.followers}</li>
+      <li>Views:{stats.views}</li>
+      <li>Likes:{stats.likes}</li>
+    </ul>
+  </div>
+);
+
+Profile.PropTypes = {
+  avatar: PropTypes.string,
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  followers: PropTypes.number,
+  views: PropTypes.number,
+  likes: PropTypes.number,
+};
+
+export default Profile;
