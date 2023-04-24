f77dce54f0103cd6d584017dd66236a9a7d576f4
Roman Lebedynskyi <108210477+Anasurim@users.noreply.github.com>
2023-04-24 19:25:30 +0200

    stat css completed

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,20 +1,30 @@
 import PropTypes from 'prop-types';
+import css from '../Statistics/Statistics.module.css';
 
 const Statistics = ({ title, stats }) => {
   return (
-    <div>
-      {title && <h2 className="title">{title}</h2>}
+    <div className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
 
-      <ul>
+      <ul className={css.statList}>
         {stats.map(stat => (
-          <li key={stat.id}>
-            <span className="label">{stat.label}</span>
-            <span className="percentage">{stat.percentage}%</span>
+          <li key={stat.id} style={{ backgroundColor: getRandomColor() }}>
+            <span className={css.label}>{stat.label}</span>
+            <span className={css.percentage}>{stat.percentage}%</span>
           </li>
         ))}
       </ul>
     </div>
   );
+};
+
+const getRandomColor = () => {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 };
 
 Statistics.propTypes = {
