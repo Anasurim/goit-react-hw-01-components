1643ce62940402cde32722a67da6ef928b2322a4
Roman Lebedynskyi <108210477+Anasurim@users.noreply.github.com>
2023-04-24 19:54:20 +0200

    friendL css completed

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,19 +1,30 @@
 import PropTypes from 'prop-types';
+import css from '../FriendList/FriendList.module.css';
 
 const FriendList = ({ friends }) => {
   return (
-    <ul>
+    <ul className={css.friendList}>
       {friends.map(friend => {
         return (
-          <li key={friend.id}>
-            <span className="status"></span>
+          <li key={friend.id} className={css.item}>
+            {friend.isOnline ? (
+              <span
+                className={css.status}
+                style={{ backgroundColor: 'green' }}
+              ></span>
+            ) : (
+              <span
+                className={css.status}
+                style={{ backgroundColor: 'red' }}
+              ></span>
+            )}
             <img
-              className="avatar"
+              className={css.avatar}
               src={friend.avatar}
               alt="User avatar"
               width="48"
             />
-            <p className="name">{friend.name}</p>
+            <p className={css.name}>{friend.name}</p>
           </li>
         );
       })}
